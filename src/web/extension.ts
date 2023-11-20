4929414a7937a27603afe35daf42d229dfd09767
jm9567s <joel.modess@alumni.fh-aachen.de>
2023-11-20 21:44:04 +0100

    button for docker mode opens the belonging catalog

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -8,7 +8,7 @@ import { DockerfilePanel } from './panels/DockerfilePanel';
 const dockerFileTypeProvider = new DockerFileTypeProvider();
 
 //	Selectable Docker Mode, initially 'Mode not selected'
-export var dockerNodeLabel: string = "[Mode not selected]"; 
+export var dockerNodeLabel: string = "[Mode not selected]";
 
 
 // This method is called when your extension is activated
@@ -19,44 +19,40 @@ export function activate(context: vscode.ExtensionContext) {
 	// // Current Panel, only one should exist at a time
 	// let currentPanel: vscode.WebviewPanel | undefined = undefined;
 
-	
+
 	// const disposableHelloWorld = vscode.commands.registerCommand('awssecplat-webextension.helloWorld', () => {
-		
+
 	// 	vscode.window.showInformationMessage('Hello World from awssecplat-webextension in a web extension host!');
 	// });
 
 	// context.subscriptions.push(disposableHelloWorld);
 
-	vscode.window.registerTreeDataProvider('dockerContainer',dockerFileTypeProvider);
+	vscode.window.registerTreeDataProvider('dockerContainer', dockerFileTypeProvider);
 
 	vscode.commands.registerCommand("dockerContainer.showDockerFileTypeView", () => {
 		vscode.commands.executeCommand('workbench.view.explorer');
 	});
-	vscode.commands.registerCommand("dockerContainer.selectContainer", (dockerNode) => {
+	const showDockerSelection = vscode.commands.registerCommand("dockerContainer.selectContainer", (dockerNode) => {
 		vscode.window.showInformationMessage((`You have selected ${dockerNode.label} as your desired Docker mode`));
 		//vscode.commands.executeCommand((`dockerContainer.selectContainer.catalog.${dockerNode.label}`));
 		dockerNodeLabel = dockerNode.label;
+
+		// Open the webview for the selected Docker Mode
+		// TODO
+		switch (dockerNodeLabel) {
+			case "Dockerfile":
+				DockerfilePanel.render(context.extensionUri);
+			case "Docker-Compose":
+				//DockerComposePanel.render(context.extensionUri);
+			case "Docker_Swarm":
+				//DockerSwarmPanel.render(context.extensionUri);
+			default:
+				vscode.window.showInformationMessage("Catalog for your " + dockerNodeLabel);
+		}
 	});
 
+	context.subscriptions.push(showDockerSelection);
 
-	const showCatalog = vscode.commands.registerCommand('test.Dockerfile', () => {
-
-		vscode.window.showInformationMessage("Catalog for your " + dockerNodeLabel);
-
-		// //Panel for catalog DockerFileType Webview
-		// currentPanel = vscode.window.createWebviewPanel(
-		// 	'catalog',
-		// 	(`${dockerNodeLabel} catalog`),
-		// 	vscode.ViewColumn.One,
-		// 	{}
-		// );
-
-		DockerfilePanel.render(context.extensionUri);
-
-	});	
-
-	context.subscriptions.push(showCatalog);
-	
 }
 
 // This method is called when your extension is deactivated

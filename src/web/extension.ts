4415411d5d28ef8953a9a16e2355b321de24514f
jm9567s <joel.modess@alumni.fh-aachen.de>
2023-12-19 00:27:43 +0100

    added docker script view button for container scanning

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -2,36 +2,34 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { DockerFileTypeProvider } from "./dockerFileType";
-import { DockerfilePanel } from './panels/DockerfilePanel';
-
-// TreeDataProvider to list up the DockerFileTypes in the view of the view container
-const dockerFileTypeProvider = new DockerFileTypeProvider();
-
-//	Selectable Docker Mode, initially 'Mode not selected'
-export var dockerNodeLabel: string = "[Mode not selected]";
+import { LanguageServices } from '../../client/src/browserClientMain';
+import { DockerModePanel } from './panels/DockerModePanel';
+import { DockerScriptViewProvider } from './dockerScriptView';
+//import { activateClient } from '../../client/src/browserClientMain';
 
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
+	// TreeDataProvider to list up the DockerFileTypes in the view of the view container
+	const dockerFileTypeProvider = new DockerFileTypeProvider();
+	const dockerScriptViewProvider = new DockerScriptViewProvider();
 
-	// // Current Panel, only one should exist at a time
-	// let currentPanel: vscode.WebviewPanel | undefined = undefined;
+	// Handler to start the Language Client 
+	const langHandler = new LanguageServices();
 
-
-	// const disposableHelloWorld = vscode.commands.registerCommand('awssecplat-webextension.helloWorld', () => {
-
-	// 	vscode.window.showInformationMessage('Hello World from awssecplat-webextension in a web extension host!');
-	// });
-
-	// context.subscriptions.push(disposableHelloWorld);
+	let dockerNodeLabel: string = "[Mode not selected]";
 
 	vscode.window.registerTreeDataProvider('dockerContainer', dockerFileTypeProvider);
+
+	vscode.window.registerTreeDataProvider('dockerScriptView', dockerScriptViewProvider);
 
 	vscode.commands.registerCommand("dockerContainer.showDockerFileTypeView", () => {
 		vscode.commands.executeCommand('workbench.view.explorer');
 	});
+
+	// !Only gets registered when the select button is pressed, so everything depending on the docker Mode needs to be executed here!
 	const showDockerSelection = vscode.commands.registerCommand("dockerContainer.selectContainer", (dockerNode) => {
 		vscode.window.showInformationMessage((`You have selected ${dockerNode.label} as your desired Docker mode`));
 		//vscode.commands.executeCommand((`dockerContainer.selectContainer.catalog.${dockerNode.label}`));
@@ -41,21 +39,46 @@ export function activate(context: vscode.ExtensionContext) {
 		// TODO
 		switch (dockerNodeLabel) {
 			case "Dockerfile":
-				DockerfilePanel.render(context.extensionUri);
+				DockerModePanel.render(context.extensionUri, dockerNodeLabel);
 				break;
 			case "Docker-Compose":
-				//DockerComposePanel.render(context.extensionUri);
+				DockerModePanel.render(context.extensionUri, dockerNodeLabel);
 				break;
 			case "Docker_Swarm":
-				//DockerSwarmPanel.render(context.extensionUri);
+				//DockerSwarmPanel.render(context.extensionUri, dockerNodeLabel);
 				break;
 		}
 		vscode.window.showInformationMessage("Catalog for your " + dockerNodeLabel);
+
+		// TODO: Store the dockerNodeLabel in the extensionContext
+		context.workspaceState.update('dockerNodeLabel', dockerNodeLabel);
+		console.log("label with context in register command: " + context.workspaceState.get('dockerNodeLabel'));
+		console.log("label in register command: " + dockerNodeLabel);
+
+		//start client, when docker type selected
+		langHandler.activateClient(context, dockerNodeLabel);
+
+	});
+
+	const showDockerScriptView = vscode.commands.registerCommand('dockerScriptView.selectContainerID', () => {
+		vscode.window.showInformationMessage((`You have selected the container scan for container <number>`));
 	});
 
 	context.subscriptions.push(showDockerSelection);
+	context.subscriptions.push(showDockerScriptView);
+
+	console.log("label outside register command: " + dockerNodeLabel);
+
+	// //if (client) {
+	// 	//const disposable = client.start();
+	// 	console.log(langHandler.getTrace());
+	// 	console.log("label with context outside register command: " + context.workspaceState.get('dockerNodeLabel') + " client: " + client);
+	// 	context.subscriptions.push(disposable);
+	// //}
 
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {
+
+}
